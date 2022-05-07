a021761c905c364ba2614af8a0e8fc6365459c1a
AtulPant2704 <atulpant1999@gmail.com>
2022-05-07 19:37:19 +0530

    feat: display boards

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { NavigationType, useNavigate } from "react-router-dom";
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Button, Heading, Text, Image } from "@chakra-ui/react";
 import { useAuth } from "../../contexts/auth-context";
+import { getBoardsService } from "../../services";
 
 const Home = () => {
-  const { currentUser } = useAuth();
   const navigate = useNavigate();
+  const [boards, setBoards] = useState([]);
+  const { currentUser } = useAuth();
 
-  // const user = currentUser && currentUser.displayName.indexOf(' ');
-  // const creds = `${currentUser && currentUser.displayName.slice(0, 1)}${
-  //   currentUser && currentUser.displayName.slice(user + 1, user + 2)
-  // }`;
+  useEffect(() => {
+    getBoardsService(setBoards);
+  }, []);
 
   return (
     <Box bg="gray.300" h="100vh">
@@ -28,10 +30,15 @@ const Home = () => {
         </Heading>
 
         <Button onClick={() => navigate("/board")}>Add new Board</Button>
-        <Button borderRadius="full" h="12" w="12" size="md">
-          {/* {currentUser && creds} */}
-          AP
-        </Button>
+        {currentUser.id ? (
+          <Image
+            src={currentUser.userImgUrl}
+            borderRadius="full"
+            h="10"
+            w="10"
+            size="md"
+          ></Image>
+        ) : null}
       </Box>
       <Box
         maxW="1400px"
@@ -45,126 +52,31 @@ const Home = () => {
         flexWrap="wrap"
         justifyContent="center"
       >
-        <Box
-          bg="whiteAlpha.900"
-          p="3"
-          w="60"
-          h="32"
-          d="flex"
-          flexDir="column"
-          boxShadow="lg"
-          borderRadius="10px"
-        >
-          <Heading as="h4" size="md">
-            Template title
-          </Heading>
-          <Box d="flex" alignItems="center" mt="auto">
-            <Text fontSize="sm" casing="capitalize" mr="auto">
-              name of owner
-            </Text>
-            <Button w="fit-content">Join</Button>
-          </Box>
-        </Box>
-        <Box
-          bg="whiteAlpha.900"
-          p="3"
-          w="60"
-          h="32"
-          d="flex"
-          flexDir="column"
-          boxShadow="lg"
-          borderRadius="10px"
-        >
-          <Heading as="h4" size="md">
-            Template title
-          </Heading>
-          <Box d="flex" alignItems="center" mt="auto">
-            <Text fontSize="sm" casing="capitalize" mr="auto">
-              name of owner
-            </Text>
-            <Button w="fit-content">Join</Button>
-          </Box>
-        </Box>
-        <Box
-          bg="whiteAlpha.900"
-          p="3"
-          w="60"
-          h="32"
-          d="flex"
-          flexDir="column"
-          boxShadow="lg"
-          borderRadius="10px"
-        >
-          <Heading as="h4" size="md">
-            Template title
-          </Heading>
-          <Box d="flex" alignItems="center" mt="auto">
-            <Text fontSize="sm" casing="capitalize" mr="auto">
-              name of owner
-            </Text>
-            <Button w="fit-content">Join</Button>
-          </Box>
-        </Box>
-        <Box
-          bg="whiteAlpha.900"
-          p="3"
-          w="60"
-          h="32"
-          d="flex"
-          flexDir="column"
-          boxShadow="lg"
-          borderRadius="10px"
-        >
-          <Heading as="h4" size="md">
-            Template title
-          </Heading>
-          <Box d="flex" alignItems="center" mt="auto">
-            <Text fontSize="sm" casing="capitalize" mr="auto">
-              name of owner
-            </Text>
-            <Button w="fit-content">Join</Button>
-          </Box>
-        </Box>
-        <Box
-          bg="whiteAlpha.900"
-          p="3"
-          w="60"
-          h="32"
-          d="flex"
-          flexDir="column"
-          boxShadow="lg"
-          borderRadius="10px"
-        >
-          <Heading as="h4" size="md">
-            Template title
-          </Heading>
-          <Box d="flex" alignItems="center" mt="auto">
-            <Text fontSize="sm" casing="capitalize" mr="auto">
-              name of owner
-            </Text>
-            <Button w="fit-content">Join</Button>
-          </Box>
-        </Box>
-        <Box
-          bg="whiteAlpha.900"
-          p="3"
-          w="60"
-          h="32"
-          d="flex"
-          flexDir="column"
-          boxShadow="lg"
-          borderRadius="10px"
-        >
-          <Heading as="h4" size="md">
-            Template title
-          </Heading>
-          <Box d="flex" alignItems="center" mt="auto">
-            <Text fontSize="sm" casing="capitalize" mr="auto">
-              name of owner
-            </Text>
-            <Button w="fit-content">Join</Button>
-          </Box>
-        </Box>
+        {boards.length > 0
+          ? boards.map((board) => (
+              <Box
+                key={board.id}
+                bg="whiteAlpha.900"
+                p="3"
+                w="60"
+                h="32"
+                d="flex"
+                flexDir="column"
+                boxShadow="lg"
+                borderRadius="10px"
+              >
+                <Heading as="h4" size="md">
+                  {board.boardTitle}
+                </Heading>
+                <Box d="flex" alignItems="center" mt="auto">
+                  <Text fontSize="sm" casing="capitalize" mr="auto">
+                    {board.adminName}
+                  </Text>
+                  <Button w="fit-content">Join</Button>
+                </Box>
+              </Box>
+            ))
+          : null}
       </Box>
     </Box>
   );
---
+++ src/services/getBoards.service.js
@@ -0,0 +1,20 @@
+import { getDocs, collection, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase.config";
+
+const getBoardsService = async (setBoards) => {
+  try {
+    const boardsCollectionRef = collection(db, "boards");
+    await getDocs(boardsCollectionRef);
+    onSnapshot(boardsCollectionRef, (response) => {
+      const demoBoards = response.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setBoards(demoBoards);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { getBoardsService };
