a2b9ce5fd33e914b7363e41bd0b831f36da53e73
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-07 21:04:19 +0530

    refactor: merge conflict resolve

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,35 +3,38 @@ import React from 'react';
 import { useAuth } from '../../contexts/auth-context';
 
 const Navbar = ({ onOpen }) => {
-  const { currentUser } = useAuth();
+	const { currentUser } = useAuth();
 
-  return (
-    <Box
-      position="fixed"
-      left="0"
-      right="0"
-      top="0"
-      p="4"
-      d="flex"
-      justifyContent="space-between"
-      bg="white"
-      boxShadow="md"
-      alignItems="center"
-      h="80px"
-      zIndex="5"
-    >
-      <Heading as="h1" size="lg">
-        BoardIt
-      </Heading>
+	return (
+		<Box
+			position="fixed"
+			left="0"
+			right="0"
+			top="0"
+			p="4"
+			d="flex"
+			justifyContent="space-between"
+			bg="white"
+			boxShadow="md"
+			alignItems="center"
+			h="80px"
+			zIndex="5"
+		>
+			<Heading as="h1" size="lg">
+				BoardIt
+			</Heading>
 
-      {onOpen ? (
-        <Button onClick={onOpen} colorScheme="twitter">
-          Add new Board
-        </Button>
-      ) : null}
-      <Avatar name={currentUser ? currentUser.displayName : ''} />
-    </Box>
-  );
+			{onOpen ? (
+				<Button onClick={onOpen} colorScheme="twitter">
+					Add new Board
+				</Button>
+			) : null}
+			<Avatar
+				name={currentUser.id ? currentUser.userName : ''}
+				src={currentUser.id && currentUser.userImgUrl}
+			/>
+		</Box>
+	);
 };
 
 export { Navbar };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,86 +1,60 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { Box, Button, Heading, Text, Image } from "@chakra-ui/react";
-import { useAuth } from "../../contexts/auth-context";
-import { getBoardsService } from "../../services";
+import { useState, useEffect } from 'react';
+import { Box, Button, Heading, Text, useDisclosure } from '@chakra-ui/react';
+import { getBoardsService } from '../../services';
+import { Navbar, NewBoardModal } from '../../components';
 
 const Home = () => {
+	const [boards, setBoards] = useState([]);
+	const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const navigate = useNavigate();
-  const [boards, setBoards] = useState([]);
-  const { currentUser } = useAuth();
+	useEffect(() => {
+		getBoardsService(setBoards);
+	}, []);
 
-   useEffect(() => {
-    getBoardsService(setBoards);
-  }, []);
-
-  return (
-    <Box bg="gray.300" h="100vh">
-      <Box
-        p="4"
-        d="flex"
-        justifyContent="space-between"
-        bg="whiteAlpha.900"
-        boxShadow="md"
-        alignItems="center"
-        h="80px"
-      >
-        <Heading as="h1" size="lg">
-          BoardIt
-        </Heading>
-
-        <Button onClick={() => navigate("/board")}>Add new Board</Button>
-        {currentUser.id ? (
-          <Image
-            src={currentUser.userImgUrl}
-            borderRadius="full"
-            h="10"
-            w="10"
-            size="md"
-          ></Image>
-        ) : null}
-      </Box>
-      <Box
-        maxW="1400px"
-        mx={{
-          md: "auto",
-          base: "12",
-        }}
-        my="8"
-        d="flex"
-        gap="5"
-        flexWrap="wrap"
-        justifyContent="center"
-      >
-        {boards.length > 0
-          ? boards.map((board) => (
-              <Box
-                key={board.id}
-                bg="whiteAlpha.900"
-                p="3"
-                w="60"
-                h="32"
-                d="flex"
-                flexDir="column"
-                boxShadow="lg"
-                borderRadius="10px"
-              >
-                <Heading as="h4" size="md">
-                  {board.boardTitle}
-                </Heading>
-                <Box d="flex" alignItems="center" mt="auto">
-                  <Text fontSize="sm" casing="capitalize" mr="auto">
-                    {board.adminName}
-                  </Text>
-                  <Button w="fit-content">Join</Button>
-                </Box>
-              </Box>
-            ))
-          : null}
-      </Box>
-    </Box>
-  );
+	return (
+		<Box bg="gray.300" h="100vh">
+			<Navbar onOpen={onOpen} />
+			<NewBoardModal isOpen={isOpen} onClose={onClose} />
+			<Box
+				maxW="1400px"
+				mx={{
+					md: 'auto',
+					base: '12',
+				}}
+				my="8"
+				d="flex"
+				gap="5"
+				flexWrap="wrap"
+				justifyContent="center"
+				pt="28"
+			>
+				{boards.length > 0
+					? boards.map((board) => (
+							<Box
+								key={board.id}
+								bg="whiteAlpha.900"
+								p="3"
+								w="60"
+								h="32"
+								d="flex"
+								flexDir="column"
+								boxShadow="lg"
+								borderRadius="10px"
+							>
+								<Heading as="h4" size="md">
+									{board.boardTitle}
+								</Heading>
+								<Box d="flex" alignItems="center" mt="auto">
+									<Text fontSize="sm" casing="capitalize" mr="auto">
+										{board.adminName}
+									</Text>
+									<Button w="fit-content">Join</Button>
+								</Box>
+							</Box>
+					  ))
+					: null}
+			</Box>
+		</Box>
+	);
 };
 export { Home };
-
