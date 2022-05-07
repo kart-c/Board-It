d88714a9a17844f44ff546f47f6154cf66bd3922
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-07 17:21:40 +0530

    feat: add modal component

---
+++ src/components/NewBoardModal/NewBoardModal.jsx
@@ -0,0 +1,46 @@
+import { useRef } from 'react';
+import {
+	Button,
+	FormControl,
+	FormLabel,
+	Input,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
+} from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+
+const NewBoardModal = ({ isOpen, onClose }) => {
+	const navigate = useNavigate();
+	const initialRef = useRef();
+
+	return (
+		<>
+			<Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+				<ModalOverlay />
+				<ModalContent mx="10">
+					<ModalHeader>Create New Board</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody pb="6">
+						<FormControl>
+							<FormLabel>Board name</FormLabel>
+							<Input ref={initialRef} placeholder="Project Management" />
+						</FormControl>
+					</ModalBody>
+					<ModalFooter>
+						<Button colorScheme="twitter" mr="3" onClick={() => navigate('/board')}>
+							Save
+						</Button>
+						<Button onClick={onClose}>Cancel</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
+	);
+};
+
+export { NewBoardModal };
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { List } from './List/List';
 export { Navbar } from './Navbar/Navbar';
+export { NewBoardModal } from './NewBoardModal/NewBoardModal';
