f62a168e05909250f61cb6fddb1263cf87ef40a9
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-05-08 12:20:25 +0530

    refactor: replace flex with grid

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,55 +1,57 @@
-import { Box, Button, Heading, Text, useDisclosure } from "@chakra-ui/react";
-import { useBoards } from "../../contexts";
-import { Navbar, NewBoardModal } from "../../components";
+import { Box, Button, Grid, GridItem, Heading, Text, useDisclosure } from '@chakra-ui/react';
+import { useBoards } from '../../contexts';
+import { Navbar, NewBoardModal } from '../../components';
 
 const Home = () => {
-  const { boards } = useBoards();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+	const { boards } = useBoards();
+	const { isOpen, onOpen, onClose } = useDisclosure();
 
-  return (
-    <Box bg="gray.300" h="100vh">
-      <Navbar onOpen={onOpen} />
-      <NewBoardModal isOpen={isOpen} onClose={onClose} />
-      <Box
-        maxW="1400px"
-        mx={{
-          md: "auto",
-          base: "12",
-        }}
-        my="8"
-        d="flex"
-        gap="5"
-        flexWrap="wrap"
-        justifyContent="center"
-        pt="28"
-      >
-        {boards.length > 0
-          ? boards.map((board) => (
-              <Box
-                key={board.id}
-                bg="whiteAlpha.900"
-                p="3"
-                w="60"
-                h="32"
-                d="flex"
-                flexDir="column"
-                boxShadow="lg"
-                borderRadius="10px"
-              >
-                <Heading as="h4" size="md">
-                  {board.boardTitle}
-                </Heading>
-                <Box d="flex" alignItems="center" mt="auto">
-                  <Text fontSize="sm" casing="capitalize" mr="auto">
-                    {board.adminName}
-                  </Text>
-                  <Button w="fit-content">Join</Button>
-                </Box>
-              </Box>
-            ))
-          : null}
-      </Box>
-    </Box>
-  );
+	return (
+		<Box bg="gray.300" minH="100vh">
+			<Navbar onOpen={onOpen} />
+			<NewBoardModal isOpen={isOpen} onClose={onClose} />
+			<Grid
+				pb="12"
+				pt="28"
+				gap="5"
+				mx={{
+					'2xl': 'auto',
+					base: '12',
+				}}
+				templateColumns={{
+					lg: 'repeat(4, 1fr)',
+					md: 'repeat(3, 1fr)',
+					sm: 'repeat(2, 1fr)',
+					base: 'repeat(1, 1fr)',
+				}}
+				maxW="1360px"
+			>
+				{boards.length > 0
+					? boards.map((board) => (
+							<GridItem
+								key={board.id}
+								bg="whiteAlpha.900"
+								p="3"
+								h="32"
+								d="flex"
+								flexDir="column"
+								boxShadow="lg"
+								borderRadius="10px"
+							>
+								<Heading as="h4" size="md">
+									{board.boardTitle}
+								</Heading>
+								<Box d="flex" alignItems="center" mt="auto">
+									<Text fontSize="sm" casing="capitalize" mr="auto">
+										{board.adminName}
+									</Text>
+									<Button w="fit-content">Join</Button>
+								</Box>
+							</GridItem>
+					  ))
+					: null}
+			</Grid>
+		</Box>
+	);
 };
 export { Home };
