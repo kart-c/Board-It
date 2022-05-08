c197adfe2f0385c9c0a1382cdafd57f394ed1850
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-05-08 19:17:22 +0530

    Feat: Add last edit by and date to add/update card to list service

--- src/services/addCardToList.service.js
+++ src/services/addCardToList.service.js
@@ -2,13 +2,21 @@ import { v4 as uuid } from "uuid";
 import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebase.config";
 
-const addCardToListService = async (board, listId, cardContent) => {
+const addCardToListService = async (
+  board,
+  listId,
+  cardContent,
+  currentUser
+) => {
   try {
     const boardDoc = doc(db, "boards", board.id);
+    const todaysDate = new Date().toDateString();
 
     const cardObj = {
       cardId: uuid(),
       cardContent: cardContent,
+      lastEditedBy: currentUser.userName,
+      lastEditTime: todaysDate,
     };
 
     let cardList = board.lists.find((item) => item.listId === listId);
--- src/services/updateCard.service.js
+++ src/services/updateCard.service.js
@@ -1,13 +1,16 @@
 import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebase.config";
 
-const updateCardService = async (board, listId, cardId, cardContent) => {
+const updateCardService = async (board, listId, cardId, cardContent, currentUser) => {
   try {
     const boardDoc = doc(db, "boards", board.id);
+    const todaysDate = new Date().toDateString();
 
     const cardObj = {
       cardId: cardId,
       cardContent: cardContent,
+      lastEditedBy: currentUser.userName,
+      lastEditTime: todaysDate,
     };
 
     let cardList = board.lists.find((item) => item.listId === listId);
