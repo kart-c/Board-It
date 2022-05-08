315002133cea9777223d034a2df7cc9ede69d529
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-05-08 21:09:43 +0530

    Fix: get Time utility function

--- src/utils/getCurrentTimeAndDate.js
+++ src/utils/getCurrentTimeAndDate.js
@@ -1,5 +1,7 @@
 export const getCurrentTimeAndDate = (str) => {
   const today = new Date();
   const date = today.toLocaleString().split(",")[0];
-  return `${date} | ${today.getHours()}:${today.getMinutes()}`;
+  return `${date} | ${today.getHours()}:${
+    today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes()
+  }`;
 };
